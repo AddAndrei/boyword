f9339cad0ccbc6f9375efcdc28d26433afbdd809
sherbakov andrei <asherbakov@nopreset.ru>
2025-08-27 11:54:58 +0300

    add cors

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\DB;
 */
 
 Route::get('/', function () {
+    try {
+        DB::connection()->getPDO();
+        echo DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        echo 'None';
+    }
     return view('welcome');
 });
